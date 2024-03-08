501e4d19bc5098f4ca57ee42f5cce395cc9a00bc
ShivaRK <shivarkcodes@gmail.com>
2024-03-08 23:54:47 +0530

    fix: assets

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './screens/Home'
 import Login from './screens/Login'
 import Signup from './screens/Signup'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}/>
     </Routes>
+    </>
   )
 }
 
