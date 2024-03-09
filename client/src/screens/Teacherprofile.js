a715268f8efbf31fa13bf26f4d6def40135c52fe
narsimha777 <kandinarsimhareddy09@gmail.com>
2024-03-09 19:28:53 +0530

    tutordashboardcmplt

--- client/src/screens/Teacherprofile.js
+++ client/src/screens/Teacherprofile.js
@@ -3,87 +3,87 @@ import { PaperClipIcon } from '@heroicons/react/20/solid'
 
 const TeacherProfile = ({ profile }) => {
   return (
-    // <div className="bg-gray-100 p-6 rounded-lg">
-    //   <h2 className="text-xl font-semibold mb-4">{profile.username}</h2>
-    //   <div className="grid grid-cols-2 gap-4">
-    //     <div>
-    //       <p className="text-gray-600 font-semibold">Username:</p>
-    //       <p className="text-gray-800">{profile.username}</p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-600 font-semibold">Email:</p>
-    //       <p className="text-gray-800">{profile.email}</p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-600 font-semibold">Age:</p>
-    //       <p className="text-gray-800">{profile.age}</p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-600 font-semibold">Experience:</p>
-    //       <p className="text-gray-800">{profile.experience}</p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-600 font-semibold">Rating:</p>
-    //       <p className="text-gray-800">{profile.rating} </p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-600 font-semibold">Description:</p>
-    //       <p className="text-gray-800">{profile.description}</p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-600 font-semibold">Number of Courses Taking:</p>
-    //       <p className="text-gray-800">{profile.courses_taking.length} </p>
-    //     </div>
-    //     <div>
-    //       <p className="text-gray-600 font-semibold">Country:</p>
-    //       <p className="text-gray-800">{profile.country}</p>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className='bg-gray-100 p-5 rounded-xl'>
-      <div className="px-4 sm:px-0">
-        <h3 className="text-base font-semibold leading-7 text-gray-900">{profile.username}</h3>
-        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details...</p>
-      </div>
-      <div className="mt-6 border-t border-gray-100">
-        <dl className="divide-y divide-gray-100">
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{profile.username}</dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Age</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{profile.age}</dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{profile.email}</dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Experience</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{profile.experience}</dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">About</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {profile.description}
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Country</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {profile.country}
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Rating</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {profile.rating} ✨
-            </dd>
-          </div>
-        </dl>
+    <div className="bg-gray-100 p-6 rounded-lg">
+      <h2 className="text-xl font-semibold mb-4">{profile.username}</h2>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <p className="text-gray-600 font-semibold">Username:</p>
+          <p className="text-gray-800">{profile.username}</p>
+        </div>
+        <div>
+          <p className="text-gray-600 font-semibold">Email:</p>
+          <p className="text-gray-800">{profile.email}</p>
+        </div>
+        <div>
+          <p className="text-gray-600 font-semibold">Age:</p>
+          <p className="text-gray-800">{profile.age}</p>
+        </div>
+        <div>
+          <p className="text-gray-600 font-semibold">Experience:</p>
+          <p className="text-gray-800">{profile.experience}</p>
+        </div>
+        <div>
+          <p className="text-gray-600 font-semibold">Rating:</p>
+          <p className="text-gray-800">{profile.rating} ✨</p>
+        </div>
+        <div>
+          <p className="text-gray-600 font-semibold">Description:</p>
+          <p className="text-gray-800">{profile.description}</p>
+        </div>
+        <div>
+          <p className="text-gray-600 font-semibold">Number of Courses Taking:</p>
+          <p className="text-gray-800">{profile.courses_taking.length} </p>
+        </div>
+        <div>
+          <p className="text-gray-600 font-semibold">Country:</p>
+          <p className="text-gray-800">{profile.country}</p>
+        </div>
       </div>
     </div>
+    // <div className='bg-gray-100 p-5 rounded-xl'>
+    //   <div className="px-4 sm:px-0">
+    //     <h3 className="text-base font-semibold leading-7 text-gray-900">{profile.username}</h3>
+    //     <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details...</p>
+    //   </div>
+    //   <div className="mt-6 border-t border-gray-100">
+    //     <dl className="divide-y divide-gray-100">
+    //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+    //         <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+    //         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{profile.username}</dd>
+    //       </div>
+    //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+    //         <dt className="text-sm font-medium leading-6 text-gray-900">Age</dt>
+    //         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{profile.age}</dd>
+    //       </div>
+    //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+    //         <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
+    //         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{profile.email}</dd>
+    //       </div>
+    //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+    //         <dt className="text-sm font-medium leading-6 text-gray-900">Experience</dt>
+    //         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{profile.experience}</dd>
+    //       </div>
+    //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+    //         <dt className="text-sm font-medium leading-6 text-gray-900">About</dt>
+    //         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+    //           {profile.description}
+    //         </dd>
+    //       </div>
+    //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+    //         <dt className="text-sm font-medium leading-6 text-gray-900">Country</dt>
+    //         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+    //           {profile.country}
+    //         </dd>
+    //       </div>
+    //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+    //         <dt className="text-sm font-medium leading-6 text-gray-900">Rating</dt>
+    //         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+    //           {profile.rating} ✨
+    //         </dd>
+    //       </div>
+    //     </dl>
+    //   </div>
+    // </div>
   );
 };
 
--- client/src/screens/TutorDashboard.js
+++ client/src/screens/TutorDashboard.js
@@ -14,6 +14,24 @@ setOptions({
 
 const TeacherDashboard = () => {
     const role = 'tutor';
+    const [duration, setDuration] = useState([]);
+    const [selectedDate30, setSelectedDate30] = useState(null);
+    const [selectedDate60, setSelectedDate60] = useState(null);
+    const [selectedDate90, setSelectedDate90] = useState(null);
+    const [validity, setValidity] = useState();
+    const [description, setDescription] = useState();
+    const [coursename, setCoursename] = useState('');
+    const [prices, setPrice] = useState({});
+    const durations = [{"value":30},{"value":60},{"value":90}] 
+    const handleDateChange30 = (event) => {
+      setSelectedDate30(event.value);
+    };  
+    const handleDateChange60 = (event) => {
+      setSelectedDate60(event.value);
+    };  
+    const handleDateChange90 = (event) => {
+      setSelectedDate90(event.value);
+    };  
     function calculateEndTime(startTime, duration) {
         const start = new Date(startTime);
         const end = new Date(start.getTime() + duration * 60000); // Convert minutes to milliseconds
@@ -59,12 +77,45 @@ const TeacherDashboard = () => {
     { price: "$150" }
   ];
 
-  function generateRandomColor() {
-    // Generate random hexadecimal color code
-    const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    return color;
+  const handleSubmit =()=>{
+    const Timespan = duration.map((ele) => {
+      if (ele.value === 30) {
+        return { "start_time": selectedDate30, "duration": ele.value };
+      }
+      if (ele.value === 60) {
+        return { "start_time": selectedDate60, "duration": ele.value };
+      }
+      if (ele.value === 90) {
+        return { "start_time": selectedDate90, "duration": ele.value };
+      }
+    });
+    const finalobject = {
+      "course_name":coursename,
+      "pricing":[
+        {
+          "30min":prices["30min"],
+          "60min":prices["60min"],
+          "90min":prices["90min"]
+        }
+      ],
+      "course_desc":description,
+      "Time_span":Timespan,
+      "valid_upto":validity,
+      "Course_level":selectedValue,
+    }
+    
   }
+  // function generateRandomColor() {
+  //   // Generate random hexadecimal color code
+  //   const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  //   return color;
+  // }
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+    console.log('Selected option value:', event.target.value);
+  };
 
+  const [selectedValue, setSelectedValue] = useState('');
   const [languageFilter, setLanguageFilter] = useState(null);
   const [experienceFilter, setExperienceFilter] = useState(null);
   const [pricingFilter, setPricingFilter] = useState(null);
@@ -87,7 +138,7 @@ const TeacherDashboard = () => {
     return profile.languages_known.map((ele, ind) => ({
       id: ind,
       name: ele.name,
-      color: generateRandomColor()
+      color: '#FF0000'
     }));
   }, [profile.languages_known]);
 
@@ -124,10 +175,107 @@ const TeacherDashboard = () => {
             
             />
                       {role === 'tutor' && <div className="pt-5"><h1><b>Update new Course</b></h1>
-                          <Datepicker
-                              controls={['datetime']}
-                              touchUi={true}
-                          /></div>}
+              <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Enter Course Name
+                  </label>
+                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" onChange={(e)=>setCoursename(e.target.value)}required />
+                </div>
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="selectOption">Enter Course-level:</label>
+                <select id="selectOption" name="selectOption" value={selectedValue} onChange={handleChange}>
+                  <option value="A1">A1</option>
+                  <option value="A2">A2</option>
+                  <option value="B1">B1</option>
+                  <option value="B2">B2</option>
+                </select>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Enter Course Validity period in Days
+                  </label>
+                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" onChange={(e)=>setValidity(e.target.value)}required />
+                </div>
+                <div className="mb-4">
+                  <div className="mb-2">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
+                      Enter Price for 30 minutes session
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value={prices[0]} onChange={(e) => { setPrice({"30min":e.target.value,"60min": prices["60min"], "90min":prices["90min"]}) }} required />
+                  </div>
+                  <div className="mb-2">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
+                      Enter Price for 60 minutes session
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value={prices[1]} onChange={(e) => {setPrice({"30min":prices["30min"],"60min": e.target.value, "90min":prices["90min"]})}} required />
+                  </div>
+                  <div className="mb-2">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
+                      Enter Price for 90 minutes session
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value={prices[2]} onChange={(e) => { setPrice({"30min":prices["30min"],"60min": prices["60min"], "90min":e.target.value})}} required />
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    {/* {description} */}
+                    Enter Course Description
+                  </label>
+                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={description} type="text" onChange={(e)=>{setDescription(e.target.value)}}required />
+                </div>
+                <div className="mb-4">
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Enter Course Duration
+                  </label>
+                  {/* {JSON.stringify(duration)} */}
+                  <Select
+                                defaultValue={[]}
+                                isMulti
+                                id='langs'
+                                name="colors"
+                                options={durations}
+                                value={duration}
+                                onChange={setDuration}
+                                // isOptionDisabled={() => selectedLang.length >= 3}
+                                getOptionLabel={(option) => option.value}
+                                className="basic-multi-select mt-2"
+                                placeholder="Select Languages"
+                                classNamePrefix="select"
+                            />
+                    {/* {duration.map((ele)=>(<><p>Enter Time-Slot for {ele.value} minute duration session</p><Datepicker
+                    controls={['datetime']}
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                    touchUi={true}
+                  /></>))} */}
+                  {duration.some(item => item.value === 30)&&<><p>Enter Time-Slot for 30 minute duration session</p><Datepicker
+                    controls={['datetime']}
+                    value={selectedDate30}
+                    onChange={handleDateChange30}
+                    touchUi={true}
+                  /></>}
+                  {duration.some(item => item.value === 60)&&<><p>Enter Time-Slot for 60 minute duration session</p><Datepicker
+                    controls={['datetime']}
+                    value={selectedDate60}
+                    onChange={handleDateChange60}
+                    touchUi={true}
+                  /></>}
+                  {duration.some(item => item.value === 90)&&<><p>Enter Time-Slot for 90 minute duration session</p><Datepicker
+                    controls={['datetime']}
+                    value={selectedDate90}
+                    onChange={handleDateChange90}
+                    touchUi={true}
+                  /></>}
+                </div>
+                <div className="flex items-center justify-between">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </form>
+
+                      </div>}
 
             {!profile.availability&&<div className="flex justify-between mt-4 items-center bg-gray-200 p-4 rounded-lg">
               <p className="text-lg text-gray-700">No upcoming lessons scheduled.</p>
@@ -158,7 +306,7 @@ const TeacherDashboard = () => {
           <h2 className="text-xl font-semibold text-gray-800 mb-4">Languages Available</h2>
           <div className="flex flex-col sm:flex-row items-center justify-around mb-8">
             {/* Language filter */}
-            {profile.languages_known.map((ele)=>(<div className="p-5 rounded-lg bg-gray-100"><div><b>{ele.name}</b></div><div><em>Rating:</em> {ele.level}</div></div>))}
+            {profile.languages_known.map((ele)=>(<div className="p-5 rounded-lg bg-gray-100"><div><b>{ele.name}</b></div><div><em>Course-level:</em> {ele.level}</div></div>))}
           </div>
 
           {/* Display Teacher Profile */}
